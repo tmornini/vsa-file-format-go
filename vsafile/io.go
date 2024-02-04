c56c95ecf5a482187cf6c49e2a197e8346ef6bac
tmornini (Tom Mornini) <tom@subledger.com>
2024-02-03 19:03:54 -0500

    add comment for reworked notes

--- vsafile/io.go
+++ vsafile/io.go
@@ -152,7 +152,62 @@ func headerFrom(reader io.Reader) (*header, error) {
 	return &h, nil
 }
 
+// EVENTS
+//     SIZE(2) DEFAULTEVENTTYPE(S)
+//     SIZE(2) COUNT(S)
+//         EVENT
+//             TRACK(2)
+//             STARTTIME(4)
+//             ENDTIME(4)
+//             STARPOSITION(4)
+//             ENDPOSITION(4)
+//             SIZE(1) TEXT(S)            <--\
+//                 "CEventBarLinear"         |
+//                     DATA(12)              | <-\
+//                 "CEventBarPulse"          |   | or
+//                     DATA(16) Pulse        | <-|
+//             NEXTEVENTTYPE(2)              |   |
+//                 FF FF = New Event Type >--/   |
+//                 01 80 = Default event type >--/
+//                 30 87 = Other event type ? perhaps observed but not understood ?
+//                 00 00 = Last event
+
+// AUDIO FILES
+//     COUNT(4)
+//         Audio Files
+//             SIZE(1) FILE(S)
+//             SIZE(1) AUDIO_DEVICE(S)
+
+// VIDEO FILES
+//     COUNT(4)
+//         Video Files
+//             SIZE(1) FILE(S)
+//             SIZE(1) AUDIO(S)
+//             SIZE(1) MONITOR(S)
+//             FULLSCREEN(1)
+//             XOFFSET(4)
+//             YOFFSET(4)
+
+// unknown3(12)
+
+// TRACK SETTINGS
+//     COUNT(4)
+//         Tracks
+//             SIZE(1) TEXT(S)
+//             ADDR(4)
+//             CNTR(1)
+//             unknown4(11)
+//             +VAL(4)
+//             -VAL(4)
+//             NEUT(4)
+//             ENBL(1)
+//             unknown5(1)
+//             unknown6(2)
+//             SIZE(1) PORT(S)
+//             unknown7(12)
+
 func eventFrom(reader io.Reader) (*event, error) {
+
 }
 
 func eventsFrom(reader io.Reader, h header) ([]event, error) {
